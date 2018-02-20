feddfd90b93f297062258a63b85b7084a296130e
이호형 <saphirus87@gmail.com>
2018-02-20 19:42:01 +0900

    상점 아이템 버튼 추가
    상점 아이템 버튼 추가

--- storeScene.cpp
+++ storeScene.cpp
@@ -14,6 +14,13 @@ storeScene::~storeScene()
 HRESULT storeScene::init()
 {
 	IMAGEMANAGER->addImage("storeScreen", ".//image//userInterface//Store.bmp", 1200, 640, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addFrameImage("storeButton", ".//image//userInterface//itemButton.bmp", 570, 81, 2, 1, true, RGB(255, 0, 255));
+
+	AddFontResourceEx(
+		"SDMiSaeng.ttf", 	// font file name
+		FR_PRIVATE,         // font characteristics
+		NULL            	// reserved
+	);
 
 	return S_OK;
 }
@@ -31,10 +38,26 @@ void storeScene::render()
 {
 	IMAGEMANAGER->render("storeScreen", getMemDC());
 
+	HFONT newFont = CreateFont(30, 0, 0, 0, 1000, 0, 0, 0, HANGUL_CHARSET, 0, 0, 0, 0, TEXT("Sandoll ¹Ì»ý"));
+	HFONT oldFont = (HFONT)SelectObject(getMemDC(), newFont);
+	SetTextColor(getMemDC(), RGB(255, 255, 255));
+
 	for (int i = 0; i < _vendorList.size(); i++)
 	{
-		TextOut(getMemDC(), 200 + (i % 3) * 150, 200 + (i / 3) * 50, _vendorList[i]->getItemName(), strlen(_vendorList[i]->getItemName()));
+		IMAGEMANAGER->frameRender("storeButton", getMemDC(), 20 + (i % 3) * 290, 220 + (i / 3) * 90, 0, 0);
+
+		TextOut(getMemDC(), 40 + (i % 3) * 290, 245 + (i / 3) * 90, _vendorList[i]->getItemName(), strlen(_vendorList[i]->getItemName()));
+
+		SetTextAlign(getMemDC(), TA_RIGHT);
+		TextOut(getMemDC(), 285 + (i % 3) * 290, 260 + (i / 3) * 90,
+			to_string(_vendorList[i]->getPrice()).c_str(), strlen(to_string(_vendorList[i]->getPrice()).c_str()));
+		SetTextAlign(getMemDC(), TA_LEFT);
 	}
+
+	SetTextColor(getMemDC(), RGB(0, 0, 255));
+	SelectObject(getMemDC(), oldFont);
+	DeleteObject(oldFont);
+	DeleteObject(newFont);
 }
 
 void storeScene::setStoreKey(string key)
@@ -45,10 +68,32 @@ void storeScene::setStoreKey(string key)
 	{
 		for (int i = 0; i < _im->getVItem().size(); i++)
 		{
-			if (_im->getVItem()[i]->getItmeKind() == ITEM_WEAPON)
+			if (_im->getVItem()[i]->getItmeKind() == ITEM_WEAPON && _im->getVItem()[i]->getPrice() != 2)
 			{
 				_vendorList.push_back(_im->getVItem()[i]);
 			}
 		}
 	}
+	else if (key == "armorStore")
+	{
+		for (int i = 0; i < _im->getVItem().size(); i++)
+		{
+			if ((_im->getVItem()[i]->getItmeKind() == ITEM_ARMOR || _im->getVItem()[i]->getItmeKind() == ITEM_HELMET
+				|| _im->getVItem()[i]->getItmeKind() == ITEM_SUB_WEAPON) && _im->getVItem()[i]->getPrice() != 2)
+			{
+				_vendorList.push_back(_im->getVItem()[i]);
+			}
+		}
+	}
+	else if (key == "consumableStore")
+	{
+		for (int i = 0; i < _im->getVItem().size(); i++)
+		{
+			if (_im->getVItem()[i]->getItmeKind() == ITEM_EXPENDABLE && _im->getVItem()[i]->getPrice() != 2)
+			{
+				_vendorList.push_back(_im->getVItem()[i]);
+			}
+		}
+	}
+
 }
